0a7b6c620a80fe37df2daad82c7068aa56768ffd
Hanu-soni <pankajsoni93444@gmail.com>
2024-03-19 12:47:40 +0530

    change_package.json

--- Zomato-frontend/src/api/index.js
+++ Zomato-frontend/src/api/index.js
@@ -1,4 +1,5 @@
- const BaseUrl = 'http://localhost:8000';
+//  const BaseUrl = 'http://localhost:8000';
+ const BaseUrl='https://zomato-mern.onrender.com';
 //const BaseUrl = 'https://zomatoclonenodeapi.azurewebsites.net';
 
 export {BaseUrl};
